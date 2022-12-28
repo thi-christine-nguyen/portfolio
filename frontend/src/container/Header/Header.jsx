a1bf7d91bfde1c15447157b94ae479c6bedd83c5
thi-christine-nguyen <thichristine@icloud.com>
2022-12-29 00:42:44 +0100

    front page

--- frontend/src/container/Header/Header.jsx
+++ frontend/src/container/Header/Header.jsx
@@ -2,31 +2,36 @@ import React from 'react';
 import './Header.scss';
 import {AppWrap} from '../../wrapper';
 
-import {images} from '../../constants';
 
 
 const Header = () => {
+
+    
     return (
-        <div className="app__header">
-            <h2 className="head-text" >Welcome to my portfolio !</h2>
-            <div className="row app__header_container">
-                
-                <div className="col app__header_text text-dark rounded border border-primary">
-                <p>
-                    Nice to meet you ! I am Thi-Christine NGUYEN <br/>
-                    I'm a IT College student from France (Montpellier) <br/>
-                    You will be able so see some of my projects ! <br/>
-                    Hope you enjoy !
-                </p>
+        <div className="align-transform">
+            <div className='row'>
+                <div className='title text-center'>
+                    <h1>Welcome to my portfolio !</h1>
+                    <div className = "desc mt-5">
+                        <h5>
+                            Nice to meet you ! I am Thi-Christine NGUYEN <br/>
+                            I am a IT College student from France (Montpellier) <br/>
+                        
+                        </h5>
+                    </div>
+                    
+
+                </div>
+                <div className="row mt-5">
+                    <div className='col text-center'>
+                        <button type="button" class="btn btn-outline-light">My resume</button>
+                    </div>
+                   
                 </div>
             </div>
-
-
-
-
-
-
+           
         </div>
+
     );
 };
 
